ca7d631e49f108052dec82d682d17ff6c0bdb907
tassaron2 <rutachigal@gmail.com>
2019-03-01 14:17:29 -0500

    added ability to specify wallpaper plugin

--- ksetwallpaper.py
+++ ksetwallpaper.py
@@ -7,18 +7,19 @@ var allDesktops = desktops();
 print (allDesktops);
 for (i=0;i<allDesktops.length;i++) {
     d = allDesktops[i];
-    d.wallpaperPlugin = "org.kde.image";
-    d.currentConfigGroup = Array("Wallpaper", "org.kde.image", "General");
+    d.wallpaperPlugin = "%s";
+    d.currentConfigGroup = Array("Wallpaper", "%s", "General");
     d.writeConfig("Image", "file://%s")
 }
 """
 
 parser = argparse.ArgumentParser(description='KDE Wallpaper setter')
 parser.add_argument('file', help='Wallpaper file name')
+parser.add_argument('--plugin', '-p', help='Wallpaper plugin (default is org.kde.image)', default='org.kde.image')
 args = parser.parse_args()
 
 
 bus = dbus.SessionBus()
 plasma = dbus.Interface(bus.get_object('org.kde.plasmashell', '/PlasmaShell'), dbus_interface='org.kde.PlasmaShell')
 
-plasma.evaluateScript(jscript % args.file)
+plasma.evaluateScript(jscript % (args.plugin, args.plugin, args.file))
